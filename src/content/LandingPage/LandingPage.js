c553dd2e48f6261715a93b768a74196f179ca7f8
Carlos Ferreira <carlosyells@yahoo.com>
2022-05-22 18:39:27 +0100

    Update LandingPage.js
    Trying to get branch 3 changes to be picked up in AWS Deploy/build

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -52,8 +52,8 @@ const LandingPage = () => {
                       What is Carbon?
                     </h2>
                     <p className="landing-page__p">
-                      Carbon is IBM’s open-source design system for digital
-                      products and experiences. With the IBM Design Language as
+                      Studio is a design system for digital
+                      products in branch react-step-3 and experiences. With the IBM Design Language as
                       its foundation, the system consists of working code,
                       design tools and resources, human interface guidelines,
                       and a vibrant community of contributors.
